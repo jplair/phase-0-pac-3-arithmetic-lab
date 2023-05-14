4afabe867586856666987b0a0eef9a5d7ccce683
jplair <127062134+jplair@users.noreply.github.com>
2023-05-14 00:53:16 -0400

    Update index.js

---
+++ index.js
@@ -0,0 +1,18 @@
+function add(a, b) {
+    return a + b;
+  }
+  function subtract(a, b) {
+    return b - a;
+  }
+  function multiply(a, b) {
+    return a * b;
+  }
+  function divide(a, b) {
+    return a / b;
+  }
+  function subtract(a, b) {
+    return a - b;
+  }
+  function increment(n) {
+    return n++;
+  }
